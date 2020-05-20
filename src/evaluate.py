5298544a222edaef2d814ceb32921276201e5adc
Сон Никита Петрович <NPSon@sberbank.ru>
2020-05-21 02:20:04 +0300

    added accuracy

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,18 +1,20 @@
 import pandas as pd
+import json
 import os
 import pickle
 import sys
 
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     sys.stderr.write('Arguments error. Usage:\n')
     sys.stderr.write('\tpython evaluate.py model_file data_valid metrics_file\n')
     sys.exit(1)
 
 model_file = sys.argv[1]
 valid_file = sys.argv[2]
-metrics_file = sys.argv[3]
+auc_file = sys.argv[3]
+acc_file = sys.argv[4]
 
 with open(model_file, 'rb') as fd:
     model = pickle.load(fd)
@@ -25,8 +27,17 @@ used_cols = [
 
 target = ['Survived', ]
 
-preds = model.predict_proba(valid_df[used_cols])[:,1]
-score = roc_auc_score(valid_df[target[0]], preds)
+preds = model.predict(valid_df[used_cols])
+preds_proba = model.predict_proba(valid_df[used_cols])[:,1]
+auc_score = roc_auc_score(valid_df[target[0]], preds_proba)
+acc_score = accuracy_score(valid_df[target[0]], preds)
 
-with open(metrics_file, 'w') as fd:
-    fd.write('{:4f}\n'.format(score))
+with open(auc_file, 'w') as fd:
+    fd.write('{:4f}\n'.format(auc_score))
+    #fd.write('%s\n' % json.dumps({
+    #    'auc': auc_score,
+    #    'accuracy': acc_score,
+    #}, ensure_ascii=False))
+
+with open(acc_file, 'w') as fd:
+    fd.write('{:4f}\n'.format(acc_score))
